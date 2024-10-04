c8554280826cac8e5b37a23ea14f68a4b64cb016
Eason Sun <ballentin133@outlook.com>
2024-10-04 21:51:08 +0800

    feat: alt-l alt-u alt-c

--- src/extension.ts
+++ src/extension.ts
@@ -11,12 +11,30 @@ export function activate(context: vscode.ExtensionContext) {
 
   context.subscriptions.push(
     vscode.commands.registerCommand("evil.focusOnEditor", () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      // vscode.window.showInformationMessage('Hello World from evil-mode!');
       vscode.commands.executeCommand("workbench.action.closePanel");
       vscode.commands.executeCommand("workbench.action.closeSidebar");
       vscode.commands.executeCommand("workbench.action.closeAuxiliaryBar");
+    })
+  );
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand("evil.alt-u", () => {
+      vscode.commands.executeCommand("cursorWordRight");
+      vscode.commands.executeCommand("editor.action.transformToUppercase");
+    })
+  );
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand("evil.alt-l", () => {
+      vscode.commands.executeCommand("cursorWordRight");
+      vscode.commands.executeCommand("editor.action.transformToLowercase");
+    })
+  );
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand("evil.alt-c", () => {
+      vscode.commands.executeCommand("cursorWordRight");
+      vscode.commands.executeCommand("editor.action.transformToTitlecase");
     })
   );
 }
